86b456c8394bf54326cb6fec2eb5946f4fe742ac
Hasibul Hasan <hasibul_uap@outlook.com>
2020-09-18 13:15:58 +0600

    search and banner design

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../style/home.css';
+import Banner from './Banner';
 
 function Home() {
     return (
         <div className="home">
             <h1>Home</h1>
+            <Banner/>
         </div>
     )
 }
